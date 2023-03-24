04e71bdcfd8db8e6cdd6019ae09ae3daee15a5d6
alerciosilva9 <silvaalercio@gmail.com>
2023-03-23 21:54:09 -0300

    Projeto criado

---
+++ src/main/java/com/url/encurtador/EncurtadorurlApplication.java
@@ -0,0 +1,14 @@
+package com.url.encurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+public class EncurtadorurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorurlApplication.class, args);
+	}
+
+}
